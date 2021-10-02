7abdec64a60c7be55cb28ec5c4d40b03eed8ba24
krupesh <krupesh.savaliya@kevit.io>
2021-10-02 14:44:41 +0530

    add initial project setup files

---
+++ config.py
@@ -0,0 +1,8 @@
+"""Load config variable from environment file"""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APP_PORT = os.environ.get("APP_PORT", 7001)
+DB_URL = os.environ.get("DATABASE_URL")
